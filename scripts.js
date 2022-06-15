af2f045f4e49c391c08b7747773f3416697de30f
virajgiri <bhujanggiri2@gmail.com>
2022-06-15 14:38:00 +0530

    aaded window onload

--- scripts.js
+++ scripts.js
@@ -1,24 +1,8 @@
 window.onload = function(){
 var count = 000;
 document.getElementById('score').innerHTML = "Level: 1, Score: 000";
-function jump(){
-    const runnerID = document.getElementById('runner');
-    const objectID = document.getElementById('object');
-    const gameoverID = document.getElementById('game-over');
-    console.log("runner jump");
-    console.log(elementsOverlap(runnerID, objectID)); // 👉️ true
-    if(elementsOverlap(runnerID, objectID)){
-        runnerID.style.animationPlayState = 'paused';
-        gameoverID.style.visibility = "visible";
-    }else{
-        count++;
-        document.getElementById('object').classList.add("jump");
-        document.getElementById('score').innerHTML = "Level: 1, Score:" +count;
-        setTimeout(res=>{
-            document.getElementById('object').classList.remove("jump");
-        },3000);
-    }
-}
+document.getElementById("game_body").addEventListener("click", jump);
+
 document.addEventListener('keydown',function(event){
 switch (event.key) {
     case "ArrowLeft":
@@ -40,7 +24,24 @@ switch (event.key) {
         break;
 }
 });
-
+function jump(){
+    const runnerID = document.getElementById('runner');
+    const objectID = document.getElementById('object');
+    const gameoverID = document.getElementById('game-over');
+    console.log("runner jump");
+    console.log(elementsOverlap(runnerID, objectID)); // 👉️ true
+    if(elementsOverlap(runnerID, objectID)){
+        runnerID.style.animationPlayState = 'paused';
+        gameoverID.style.visibility = "visible";
+    }else{
+        count++;
+        document.getElementById('object').classList.add("jump");
+        document.getElementById('score').innerHTML = "Level: 1, Score:" +count;
+        setTimeout(res=>{
+            document.getElementById('object').classList.remove("jump");
+        },3000);
+    }
+}
 function elementsOverlap(el1, el2) {
     const domRect1 = el1.getBoundingClientRect();
     const domRect2 = el2.getBoundingClientRect();
